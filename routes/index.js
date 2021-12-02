35488c91143d65f02f7a785f4251f1271f1e781e
Allion Benjamin <benjamin.allion.pro@gmail.com>
2021-12-02 17:01:20 +0100

    feat(search): new global search service + comments for meetup presentation.

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const { getAtlasSearchResults, getAllCustomers } = require('../services/customerServices')
+const { getSearchResults, getGlobalSearchResults, getAllCustomers } = require('../services/customerServices')
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-  const searchResult = await getAllCustomers();
+  const searchRequest = await getAllCustomers();
   res.render(
       'index', {
-        searchResult,
-        mongoAggregation: 'No $search query to display.',
-        searchForm: {}
+          searchResult: searchRequest.result || [],
+          mongoAggregation: 'No $search query to display.',
+          globalSearchForm: {},
+          searchForm: {}
       }
   );
 });
 
 router.post('/search', async (req, res, next) => {
   const searchFormValues = req.body;
-  const searchRequest = await getAtlasSearchResults(searchFormValues);
-  console.log(searchRequest.result);
+  const searchRequest = await getSearchResults(searchFormValues);
   res.render(
       'index', {
-        searchResult : searchRequest.result,
-        mongoAggregation: searchRequest.mongoAggregation,
-        searchForm: searchFormValues
+          searchResult : searchRequest.result || [],
+          mongoAggregation: searchRequest.mongoAggregation,
+          globalSearchForm: {},
+          searchForm: searchFormValues,
       }
   );
+});
+
+router.post('/global-search', async (req, res, next) => {
+    const searchFormValue = req.body.globalSearchFormTerm;
+    const searchRequest = await getGlobalSearchResults(searchFormValue);
+    console.log(searchRequest.result);
+    res.render(
+        'index', {
+            searchResult : searchRequest.result || [],
+            mongoAggregation: searchRequest.mongoAggregation,
+            globalSearchForm: {
+                searchFormValue
+            },
+            searchForm: {},
+        }
+    );
 });
 
 module.exports = router;
--- services/customerServices.js
+++ services/customerServices.js
@@ -1,14 +1,16 @@
 const { getMongoCollection } = require('./mongoServices')
 
 const DEFAULT_CUSTOMERS_LIMIT = 30;
-const SEARCH_INDEX_NAME = 'default'
+const SEARCH_INDEX_NAME = 'default';
 
 /**
  * Return customers from database without doing any search request
  */
 const getAllCustomers = async() => {
     const mongoCollection = await getMongoCollection();
-    return mongoCollection.find({}).limit(DEFAULT_CUSTOMERS_LIMIT).toArray()
+    return {
+        result: await mongoCollection.find({}).limit(DEFAULT_CUSTOMERS_LIMIT).toArray()
+    }
 }
 
 /**
@@ -16,7 +18,7 @@ const getAllCustomers = async() => {
  * @param {{postalCode: string, phoneNumber: string, firstName: string, lastName: string, city: string, email: string }} searchFormValues
  * @returns {Promise<{result: *, mongoAggregation: [{$search: {highlight: {path: string[]}, index: string, compound: {must: ({text: {path: *, query: *}}|{text: {path: string, query: string, fuzzy: {maxEdits: number}}})[]}}}, {$limit: number}, {$set: {score: {$meta: string}, highlights: {$meta: string}}}]}|*>}
  */
-const getAtlasSearchResults = async(searchFormValues) => {
+const getSearchResults = async(searchFormValues) => {
     const mongoCollection = await getMongoCollection();
     const searchPipeline = getSearchPipelineFromSearchForm(searchFormValues);
 
@@ -26,6 +28,8 @@ const getAtlasSearchResults = async(searchFormValues) => {
     const mongoAggregation = [
         { $search: searchPipeline },
         { $limit: DEFAULT_CUSTOMERS_LIMIT }
+                                                                                                                          // { $set: { score: { $meta: 'searchScore' } } }
+                                                                                                                          // { $set: { score: { $meta: 'searchScore' }, highlights: { "$meta": 'searchHighlights' }} }
     ]
 
     console.log(searchPipeline.compound.must) // Debug line
@@ -51,11 +55,11 @@ const getSearchPipelineFromSearchForm = (searchFormValues) => {
                 getQueryForPath('addresses.city', searchFormValues.city?.trim()),
                 getQueryForPath('phoneNumbers', searchFormValues.phoneNumber?.trim()),
                 getQueryForPath('addresses.postalCode', searchFormValues.postalCode?.trim())
+                                                                                                                                // getPhoneNumberQuery(searchFormValues),
+                                                                                                                                // getPostalCodeQuery(searchFormValues)
             ].filter( query => { return !!query })
         },
-        highlight: {
-            path: ['postalCodes', 'firstName', 'lastName']
-        }
+                                                                                                                                // highlight: { path: ['postalCodes', 'firstName', 'lastName']}
     }
 }
 
@@ -75,7 +79,99 @@ const getQueryForPath = (path, value) => {
     }
 }
 
+/**
+ * Return Mongo $search pipeline from single query value
+ * @param searchValue
+ * @returns {Promise<{result: *, mongoAggregation: [{$search: {highlight: {path: string[]}, index: string, compound: {must: {text: {path: *, query: *}}[]}}}, {$limit: number}]}|*>}
+ */
+const getGlobalSearchResults = async(searchValue) => {
+    const isGlobalSearchFormEmpty = (!searchValue || searchValue.trim().length === 0)
+    if(isGlobalSearchFormEmpty) { return getAllCustomers() }
+
+    const mongoCollection = await getMongoCollection();
+    const searchPipeline = getGlobalSearchPipelineFromTerm(searchValue);
+
+    const mongoAggregation = [
+        { $search: searchPipeline },
+        { $limit: DEFAULT_CUSTOMERS_LIMIT },
+                                                                                                                        // { $set: { score: { $meta: 'searchScore' } } }
+                                                                                                                        // { $set: { score: { $meta: 'searchScore' }, highlights: { "$meta": 'searchHighlights' }} }
+    ]
+
+    console.log(searchPipeline.compound.should) // Debug line
+    return {
+        mongoAggregation,
+        result: await mongoCollection.aggregate(mongoAggregation).toArray()
+    }
+}
+
+/**
+ * Return Mongo $search pipeline from single search term
+ * @param {string} searchValue
+ * @returns {{highlight: {path: string[]}, index: string, compound: {must: {text: {path: *, query: *}}[]}}}
+ */
+const getGlobalSearchPipelineFromTerm = (searchValue) => {
+    return {
+        index: SEARCH_INDEX_NAME,
+        compound: {
+            should: [
+                getQueryForPath('firstName', searchValue.trim()),
+                getQueryForPath('lastName', searchValue.trim()),
+                getQueryForPath('addressesStr', searchValue.trim()),
+                getQueryForPath('phoneNumbers', searchValue.trim())
+            ]
+        },
+        highlight: {
+            path: ['postalCodes', 'firstName', 'addressesStr', 'lastName']
+        }
+    }
+}
+
+/**
+ * Return 'must' query for search pipeline
+ * @param {string} path
+ * @param {string} value
+ * @param {number} boostValue
+ * @return {{text: {path: *, query: *}}}
+ */
+const getBoostQueryForPath = (path, value, boostValue) => {
+    if(!value) { return undefined }
+    return {
+        text: {
+            query: value,
+            path,
+            score: { boost: { value: boostValue } }
+        }
+    }
+}
+
+const getPostalCodeQuery = (searchFormParams) => {
+    return searchFormParams.postalCode ? {
+        text: {
+            query: searchFormParams.postalCode.trim(),
+            path: 'postalCodes',
+            fuzzy: {
+                maxEdits: 2,
+                prefixLength: 2
+            }
+        }
+    } : undefined;
+}
+
+const getPhoneNumberQuery = (searchFormParams) => {
+    return searchFormParams.phoneNumber ? {
+        text: {
+            query: searchFormParams.phoneNumber.trim(),
+            path: 'phoneNumbers',
+            fuzzy: {
+                maxEdits: 2
+            }
+        }
+    } : undefined;
+}
+
 module.exports = {
-    getAtlasSearchResults,
+    getSearchResults,
+    getGlobalSearchResults,
     getAllCustomers
 }
